a27a6e78d86a3cefda1c6d7c197cc5bcd87efe12
baiyingwaming <baiyingwaming@foxmail.com>
2025-05-09 14:14:04 +0800

    23

--- src/main/java/com/example/demo/util/JNDIlookupUtil.java
+++ src/main/java/com/example/demo/util/JNDIlookupUtil.java
@@ -1,45 +1,45 @@
-package com.example.demo.util;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
-public class JNDIlookupUtil {
-//    public static final String tomcatContext ="java:comp/env/jndi/";
-
-    public static final String tomcatContext = "java:comp/env/";
-
-//    public static final String wasContext = tomcatContext + "wasContext";
-
-    public static final String wasContext = "";
-
-    public static String getJndiVal(String jndiName) {
-        Context initCtx = null;
-
-        try {
-            initCtx = new InitialContext();
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
-
-
-        try {
-            Context envCtx = (Context) initCtx.lookup(wasContext);
-            return ((String) envCtx.lookup(jndiName)).trim();
-        } catch (NamingException e) {
-            Context envCtx = null;
-            try {
-                envCtx = (Context) initCtx.lookup(tomcatContext);
-                return  ((String) envCtx.lookup(jndiName)).trim();
-            } catch (NamingException ex) {
-                throw new RuntimeException(ex);
-            }
-
-        }
-
-
-
-    }
-
-
-}
+//package com.example.demo.util;
+//
+//import javax.naming.Context;
+//import javax.naming.InitialContext;
+//import javax.naming.NamingException;
+//
+//public class JNDIlookupUtil {
+////    public static final String tomcatContext ="java:comp/env/jndi/";
+//
+//    public static final String tomcatContext = "java:comp/env/";
+//
+////    public static final String wasContext = tomcatContext + "wasContext";
+//
+//    public static final String wasContext = "";
+//
+//    public static String getJndiVal(String jndiName) {
+//        Context initCtx = null;
+//
+//        try {
+//            initCtx = new InitialContext();
+//        } catch (NamingException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//        try {
+//            Context envCtx = (Context) initCtx.lookup(wasContext);
+//            return ((String) envCtx.lookup(jndiName)).trim();
+//        } catch (NamingException e) {
+//            Context envCtx = null;
+//            try {
+//                envCtx = (Context) initCtx.lookup(tomcatContext);
+//                return  ((String) envCtx.lookup(jndiName)).trim();
+//            } catch (NamingException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//
+//        }
+//
+//
+//
+//    }
+//
+//
+//}
